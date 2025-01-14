91b08c74933e6c2003bff2d0df8dac91747f4459
olawal023 <olawal023@student.wethinkcode.co.za>
2025-01-14 15:54:13 +0200

    FE: Update auth resource.ts and amplify.yml
    - change how env is imported

--- frontend/sptfy-to-ytmusic/amplify/auth/resource.ts
+++ frontend/sptfy-to-ytmusic/amplify/auth/resource.ts
@@ -4,10 +4,16 @@ import { referenceAuth } from '@aws-amplify/backend';
  * Define and configure your auth resource
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
  */
+const getUserPoolId = () => process.env.VITE_USER_POOL_ID || '';
+const getIdentityPoolId = () => process.env.VITE_IDENTITY_POOL_ID || '';
+const getAuthRoleArn = () => process.env.VITE_AUTH_ROLE_ARN || '';
+const getUnauthRoleArn = () => process.env.VITE_UNAUTH_ROLE_ARN || '';
+const getUserPoolClientId = () => process.env.VITE_USER_POOL_CLIENT_ID || '';
+
 export const auth = referenceAuth({
-  userPoolId: import.meta.env.VITE_USER_POOL_ID as string,
-  identityPoolId: import.meta.env.VITE_IDENTITY_POOL_ID as string,
-  authRoleArn: import.meta.env.VITE_AUTH_ROLE_ARN as string,
-  unauthRoleArn: import.meta.env.VITE_UNAUTH_ROLE_ARN as string,
-  userPoolClientId: import.meta.env.VITE_USER_POOL_CLIENT_ID as string,
+  userPoolId: getUserPoolId(),
+  identityPoolId: getIdentityPoolId(),
+  authRoleArn: getAuthRoleArn(),
+  unauthRoleArn: getUnauthRoleArn(),
+  userPoolClientId: getUserPoolClientId(),
 });
